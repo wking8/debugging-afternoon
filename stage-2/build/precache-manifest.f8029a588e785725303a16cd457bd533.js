24080497e1db416c571f5963978657126d29cfb9
Wyatt King <wyattking@Wyatts-MacBook-Pro.local>
2019-07-16 15:11:41 -0600

    Finished stage-2

---
+++ stage-2/build/precache-manifest.f8029a588e785725303a16cd457bd533.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1528570db5162209464c6363fbade323",
+    "url": "/static/media/dev-mountain-logo.1528570d.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8409cc7f8d8ed60195ed",
+    "url": "/static/js/main.269e11cb.chunk.js"
+  },
+  {
+    "revision": "88908b9f19f5b7a87509",
+    "url": "/static/js/2.15874c79.chunk.js"
+  },
+  {
+    "revision": "8409cc7f8d8ed60195ed",
+    "url": "/static/css/main.7d25bdf7.chunk.css"
+  },
+  {
+    "revision": "751f7d946dbc426194502725a8300904",
+    "url": "/index.html"
+  }
+];
--- stage-2/src/App.js
+++ stage-2/src/App.js
@@ -5,7 +5,7 @@ import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
 
 class App extends Component {
-  constuctor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       products: [],
@@ -21,7 +21,7 @@ class App extends Component {
       .get("https://practiceapi.devmountain.com/products/")
       .then(response => {
         this.setState({
-          products: response
+          products: response.data 
         });
       });
   }
